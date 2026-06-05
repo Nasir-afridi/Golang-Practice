ea1c065d442ef2f7507e8c12f427dad8bf62ecce
Nasir-afridi <nasirwc05@gmail.com>
2026-06-06 00:02:36 +0500

    added the while loop

---
+++ 05_forLoop/loop.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// For -> only construct in golang for looping. we can create while loop with the help of for loop.
+
+	// while loop
+	i := 0
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+}
