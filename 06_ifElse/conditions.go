1c551ca06aa83b6ceecc90c0355690c96579c4b2
Nasir-afridi <nasirwc05@gmail.com>
2026-06-06 00:41:47 +0500

    added the else if and  logical operators

--- 06_ifElse/conditions.go
+++ 06_ifElse/conditions.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	age := 16
+	age := 10
 
 	if age >= 18 {
 		fmt.Println("person is an adult")
+	} else if age >= 12 {
+		fmt.Println("Person is teenager")
 	} else {
-		fmt.Println("Not and adult")
+		fmt.Println("kid")
+	}
+
+	var role = "admin"
+	var hasPermissions = true
+
+	// Logical Operators
+	// Or Operator
+	if role == "admin" || hasPermissions {
+		fmt.Println("yes")
 	}
 }
