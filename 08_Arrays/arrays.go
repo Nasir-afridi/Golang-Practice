ca631bc1d90535f99fe5744ec39243e9c7b70515
Nasir-afridi <nasirwc05@gmail.com>
2026-06-09 23:56:23 +0500

    declared the array and get its length

---
+++ 08_Arrays/arrays.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Array length will be defined by us.
+	// after the nums we defined how many elements are in it.
+
+	var nums [4]int        // declared the array.
+	fmt.Println(len(nums)) // get the length of the array
+}
