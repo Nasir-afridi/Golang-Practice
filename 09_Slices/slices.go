fd4b59230d0b7346090efb42a6c1acb64ecd5e3b
Nasir-afridi <nasirwc05@gmail.com>
2026-06-10 21:15:04 +0500

    get the length of the slice

--- 09_Slices/slices.go
+++ 09_Slices/slices.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	// slice -> dynamic arrays means not fixed size like arrays.
-	var nums []int // declared slice but it is uninitialized slice and it is nil.
 
+	var nums []int // declared slice but it is uninitialized slice and it is nil.
 	fmt.Println(nums == nil)
+	fmt.Println(len(nums)) // checked the length of the slice
 }
