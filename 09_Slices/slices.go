64d92909daa2557a4a5406483b3f59b80199150b
Nasir-afridi <nasirwc05@gmail.com>
2026-06-11 13:52:54 +0500

    added the slice with make function

--- 09_Slices/slices.go
+++ 09_Slices/slices.go
@@ -8,4 +8,8 @@ func main() {
 	var nums []int // declared slice but it is uninitialized slice and it is nil.
 	fmt.Println(nums == nil)
 	fmt.Println(len(nums)) // checked the length of the slice
+
+	// slice with make function.
+	var num = make([]int, 2)
+	fmt.Println(num)
 }
